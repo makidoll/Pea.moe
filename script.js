2e63d65f77f5e67caa58f8a43917157fdef60b3f
Peacockli <37348328+Peacockli@users.noreply.github.com>
2018-03-16 03:03:07 +0100

    meh

--- script.js
+++ script.js
@@ -66,9 +66,9 @@ setTimeout(function(){
 // scrolling title
 
 var scrl = "AAAAaaaaaAAaaAaAAAAAaaaAAAAAAaaaaAAAAAAAaaaaaAAAaAaaAaAaaaAAaaaaa";
-function scrolltitle() {
+function scrollTitle() {
 	scrl = scrl.substring(1, scrl.length) + scrl.substring(0, 1);
 	document.title = scrl;
-	setTimeout("scrolltitle()", 300);
+	setTimeout("scrollTitle()", 300);
 }
-scrolltitle();
+scrollTitle();
