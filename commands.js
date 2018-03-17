f7bb3879500844ec702ecac7c7fb606ad1e888ad
Peacockli <37348328+Peacockli@users.noreply.github.com>
2018-03-17 05:29:12 +0100

    butts

--- commands.js
+++ commands.js
@@ -50,9 +50,18 @@ var commands = {
 			hueDegree = c.msg;
 			hueCommand();
 			c.print("color set to "+c.msg+" degrees")
+		}		
+	},
+	"playall": {
+		desc: "Plays all animations",
+		action: function(c) {
+			akarin(); tomato(); door();
+			c.print("I hope you're not wearing headphones")
+
 		}
 	},
 	"hello": {
+		alias: ["hi", "hey"],
 		hidden: true,
 		action: function(c) {
 			c.print("hi! :)")
@@ -61,15 +70,23 @@ var commands = {
 	"same": {
 		hidden: true,
 		action: function(c) {
-			c.print("same")
+			if (Math.floor((Math.random() * 5) + 1) > 1){
+				c.print("same")
+			}
+			else {
+				c.print("butts")
+			}
 		}
 	},
-	"playall": {
-		desc: "Plays all animations",
+	"coinflip": {
+		hidden: true,
 		action: function(c) {
-			akarin(); tomato(); door();
-			c.print("I hope you're not wearing headphones")
-
+			if (Math.floor(Math.random() * 2) == 0){
+				c.print("heads")
+			}
+			else {
+				c.print("tails")
+			}
 		}
 	}
 }
@@ -82,8 +99,13 @@ var terminal = new Term({
 		let obj = {};
 		for (var i=0; i<Object.keys(commands).length; i++) {
 			let key = Object.keys(commands)[i];
+			if (commands[key].alias) {
+				for (var j=0; j<commands[key].alias.length; j++) {
+					obj[commands[key].alias[j]] = commands[key].action;
+				}
+			}
 			obj[key] = commands[key].action;
-		}; return obj;	
+		}; return obj;    
 	})()
 });
 
--- term.js
+++ term.js
@@ -13,7 +13,23 @@ var Term = function(obj) {
 	// set child properties
 	this.child.output.style.overflowY = "auto";
 	this.child.output.style.whiteSpace = "pre";
-	this.child.input.autoComplete = "off";
+	this.child.input.autocomplete = "off";
+	this.child.input.spellcheck = false;
+
+	// term history
+	this.history = [];
+	this.historyIndex = -1;
+
+	this.getHistory = function() {
+		return this.history[this.history.length-this.historyIndex];
+	}
+
+	this.setCursorToEnd = function() {
+		setTimeout(function() { 
+			this.child.input.selectionStart = 
+			this.child.input.selectionEnd = this.child.input.value.length;
+		}.bind(this), 0);
+	}
 
 	// term functions
 	this.charify = function(msg) {
@@ -40,24 +56,39 @@ var Term = function(obj) {
 	if (obj.motd) this.print(obj.motd);
 
 	// set child events
-	this.child.input.addEventListener("keyup", function(e) {
-		if (e.keyCode == 13) { // enter
-			try {
-				let cmd = this.child.input.value.split(" ")[0];
-				this.cmds[cmd]({
-					// remember these!
-					print: this.print.bind(this),
-					msg: this.child.input.value.slice(cmd.length+1),
-					clear: function() { this.child.output.innerHTML = "" }.bind(this),
-					bold: this.bold,
-					color: this.color
-				});
-			} catch(err) {
-				this.print(this.error);
-				console.log(err);
-			}
+	this.child.input.addEventListener("keydown", function(e) {
+		switch (e.keyCode) {
+			case 38: // up
+				if (this.historyIndex<this.history.length) this.historyIndex++;
+				if (this.getHistory()) this.child.input.value = this.getHistory();
+				this.setCursorToEnd();
+				break;
+			case 40: // down
+				if (this.historyIndex>1) this.historyIndex--;
+				if (this.getHistory()) this.child.input.value = this.getHistory();
+				this.setCursorToEnd();
+				break;
+			case 13: // enter
+				this.history.push(this.child.input.value);
+				this.historyIndex = 0;
 
-			this.child.input.value = "";
+				try {
+					let cmd = this.child.input.value.split(" ")[0].toLowerCase();
+					this.cmds[cmd]({
+						// remember these!
+						print: this.print.bind(this),
+						msg: this.child.input.value.slice(cmd.length+1),
+						clear: function() { this.child.output.innerHTML = "" }.bind(this),
+						bold: this.bold,
+						color: this.color
+					});
+				} catch(err) {
+					this.print(this.error);
+					console.log(err);
+				}
+
+				this.child.input.value = "";
+				break;
 		}
 	}.bind(this));
 
